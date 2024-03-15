b03787334598219ee10aa28bb034908ef985aaad
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-03-14 21:35:46 -0600

    Remove template styling and elements

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
 import "./style.css";
-import iconSvg from "./img/erick-notepad.svg";
-
-const icon = document.createElement("span");
-icon.className = "icon";
-icon.innerHTML = iconSvg;
-
-document.body.appendChild(icon);
-
-console.log("Welcome to your new project!");
