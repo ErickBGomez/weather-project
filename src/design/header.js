97340c50351e121bb5d51fc916e2cc47027eae7d
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-03-15 22:05:26 -0600

    Add header elements

---
+++ src/design/header.js
@@ -0,0 +1,54 @@
+import searchSvg from "../img/search.svg";
+import configSvg from "../img/config.svg";
+
+function createAppTitle() {
+  const title = document.createElement("h1");
+  title.id = "app-title";
+  title.textContent = "The Weather App";
+
+  return title;
+}
+
+function createChooseAreaInput(id, placeholder) {
+  const container = document.createElement("div");
+  container.className = "input-container";
+
+  const input = document.createElement("input");
+  input.type = "text";
+  input.id = id;
+  input.name = id;
+  input.placeholder = placeholder;
+
+  const button = document.createElement("button");
+  button.id = "choose-area-button";
+  button.innerHTML = searchSvg;
+
+  container.appendChild(input);
+  container.appendChild(button);
+
+  return container;
+}
+
+function createHeaderButtons() {
+  const container = document.createElement("div");
+  container.className = "header-buttons";
+
+  const configButton = document.createElement("button");
+  configButton.className = "config-button";
+  configButton.innerHTML = configSvg;
+
+  container.appendChild(configButton);
+
+  return container;
+}
+
+function renderHeader() {
+  const header = document.createElement("header");
+  header.appendChild(createAppTitle());
+  header.appendChild(createChooseAreaInput("choose-area", "Choose area..."));
+  header.appendChild(createHeaderButtons());
+
+  document.body.appendChild(header);
+}
+
+export default renderHeader;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import "./style.css";
+import renderHeader from "./design/header";
+
+renderHeader();
