3b2b5d74274f1c6ecbf51cb1a258c7b0e4560be4
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-03-16 12:07:12 -0600

    Get current location forecast

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@ import "./style.css";
 import renderHeader from "./design/header";
 import renderFooter from "./design/footer";
 
+const currentLocation = "San Salvador";
+let forecastInfo;
+
+async function fetchForecast() {
+  const responseFetch = await fetch(
+    `https://api.weatherapi.com/v1/forecast.json?key=d9bcc94c28e04844af1222420240303&q=${currentLocation}&days=3&aqi=no&alerts=no`,
+  );
+  return responseFetch.json();
+}
+
+forecastInfo = await fetchForecast();
+
+console.log(forecastInfo);
+
 renderHeader();
 renderFooter();
