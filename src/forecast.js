e9625c7cefccf58e77a66f5fcec5c20b65d699f0
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-03-16 12:47:47 -0600

    Add location and current temperature values to DOM

--- src/forecast.js
+++ src/forecast.js
@@ -1,29 +1,67 @@
-const currentLocation = "San Salvador";
 let forecastInfo;
 
-async function fetchForecast() {
+async function fetchForecast(location) {
   const responseFetch = await fetch(
-    `https://api.weatherapi.com/v1/forecast.json?key=d9bcc94c28e04844af1222420240303&q=${currentLocation}&days=3&aqi=no&alerts=no`,
+    `https://api.weatherapi.com/v1/forecast.json?key=d9bcc94c28e04844af1222420240303&q=${location}&days=3&aqi=no&alerts=no`,
   );
   return responseFetch.json();
 }
 
-forecastInfo = await fetchForecast();
+function createCurrentWeather(location, current) {
+  const container = document.createElement("section");
+  container.id = "current-weather";
 
-console.log(forecastInfo);
+  const locationContainer = document.createElement("div");
+  const locationName = document.createElement("p");
+  const locationCountry = document.createElement("span");
 
-function renderMain() {
-  const main = document.createElement("main");
+  const currentTempContainer = document.createElement("div");
+  const temperatureValue = document.createElement("span");
+  const condition = document.createElement("p");
 
-  document.body.appendChild(main);
+  // Location
+  locationContainer.className = "location";
+  locationName.className = "name";
+  locationName.textContent = location.name;
+  locationCountry.className = "country small-text";
+  locationCountry.textContent = location.country;
+
+  locationContainer.appendChild(locationName);
+  locationContainer.appendChild(locationCountry);
+
+  // Current Temperature and Condition
+  currentTempContainer.className = "current-temp";
+  temperatureValue.className = "value";
+  temperatureValue.textContent = `${current.temp_c}°C`;
+  condition.className = "condition";
+  condition.textContent = current.condition.text;
+
+  currentTempContainer.appendChild(temperatureValue);
+  currentTempContainer.appendChild(condition);
+
+  container.appendChild(locationContainer);
+  container.appendChild(currentTempContainer);
+
+  return container;
 }
 
-function renderForecast() {
+async function renderForecast() {
   const main = document.querySelector("main");
   const container = document.createElement("div");
+  forecastInfo = await fetchForecast("San Salvador");
+
   container.id = "forecast";
+
+  container.appendChild(
+    createCurrentWeather(forecastInfo.location, forecastInfo.current),
+  );
 
   main.appendChild(container);
 }
 
-export { renderMain, renderForecast };
+function renderMain() {
+  const main = document.createElement("main");
+  document.body.appendChild(main);
+}
+
+export { renderForecast, renderMain };
