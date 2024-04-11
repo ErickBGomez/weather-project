e2029c8a4cf47201da5af6f2a4526ce0e622d1cd
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-04-10 20:59:45 -0600

    Move time conversion functions to time.js

--- src/forecast.js
+++ src/forecast.js
@@ -13,6 +13,7 @@ import loadingSvg from "./img/ui/loading.svg";
 import { readSettings } from "./settings";
 import getConditionIcon from "./conditions";
 import getMoonPhaseIcon from "./moon-phases";
+import * as time from "./time";
 
 let weather;
 
@@ -68,49 +69,14 @@ function getMagnitude(
   return `${selectedMagnitude.value} ${selectedMagnitude.units}`;
 }
 
-// From 24 hours to 12 hours
-function convertTo12Hour(time, timePattern) {
-  // Avoid if time is already 24 hours format
-  if (timePattern.test(time)) return time;
-
-  // eslint-disable-next-line prefer-const
-  let [hours, minutes] = time.split(":");
-  const modifier = hours < "12" ? "AM" : "PM";
-
-  if (hours === "00") hours = "12";
-  else if (hours > "12") hours = +hours - 12;
-
-  if (hours < 10) hours = `0${+hours}`; // Add extra 0 from 1:00 to 9:00
-
-  return `${hours}:${minutes} ${modifier}`;
-}
-
-// From 12 hours to 24 hours
-function convertTo24Hour(time, timePattern) {
-  // Avoid if time is already 12 hours format
-  if (!timePattern.test(time)) return time;
-
-  const [timeValue, modifier] = time.split(" ");
-  // eslint-disable-next-line prefer-const
-  let [hours, minutes] = timeValue.split(":");
-
-  if (modifier === "AM" && hours === "12") {
-    hours = 0;
-  } else if (modifier === "PM") {
-    if (hours !== "12") hours = +hours + 12;
-  }
-
-  return `${hours}:${minutes}`;
-}
-
-function getTime(time, timeSettings) {
+function getTime(timeValue, timeSettings) {
   const timePattern = /((1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm]))/;
   let convertedTime;
 
   if (timeSettings === "12h") {
-    convertedTime = convertTo12Hour(time, timePattern);
+    convertedTime = time.convertTo12Hour(timeValue, timePattern);
   } else if (timeSettings === "24h") {
-    convertedTime = convertTo24Hour(time, timePattern);
+    convertedTime = time.convertTo24Hour(timeValue, timePattern);
   }
 
   return convertedTime;
---
+++ src/time.js
@@ -0,0 +1,36 @@
+// From 24 hours to 12 hours
+function convertTo12Hour(time, timePattern) {
+  // Avoid if time is already 24 hours format
+  if (timePattern.test(time)) return time;
+
+  // eslint-disable-next-line prefer-const
+  let [hours, minutes] = time.split(":");
+  const modifier = hours < "12" ? "AM" : "PM";
+
+  if (hours === "00") hours = "12";
+  else if (hours > "12") hours = +hours - 12;
+
+  if (hours < 10) hours = `0${+hours}`; // Add extra 0 from 1:00 to 9:00
+
+  return `${hours}:${minutes} ${modifier}`;
+}
+
+// From 12 hours to 24 hours
+function convertTo24Hour(time, timePattern) {
+  // Avoid if time is already 12 hours format
+  if (!timePattern.test(time)) return time;
+
+  const [timeValue, modifier] = time.split(" ");
+  // eslint-disable-next-line prefer-const
+  let [hours, minutes] = timeValue.split(":");
+
+  if (modifier === "AM" && hours === "12") {
+    hours = 0;
+  } else if (modifier === "PM") {
+    if (hours !== "12") hours = +hours + 12;
+  }
+
+  return `${hours}:${minutes}`;
+}
+
+export { convertTo12Hour, convertTo24Hour };
