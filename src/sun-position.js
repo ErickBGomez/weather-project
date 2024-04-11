1926c6f59f06b8764ad839a1576aab003b0ca291
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-04-10 21:46:01 -0600

    Finish get sun positioon icon function

--- src/sun-position.js
+++ src/sun-position.js
@@ -1,19 +1,26 @@
 import * as time from "./time";
 
-function getSunPositionPercentage(sunrise, sunset, currentTime) {
+function getSunPositionIndex(sunrise, sunset, currentTime) {
   // Parse all time values to decimal number
   sunrise = time.parseTimeToNumber(sunrise);
   sunset = time.parseTimeToNumber(sunset);
   currentTime = time.parseTimeToNumber(currentTime);
 
-  // Return percentage of current position of the sun between sunrise and sunset
-  return (currentTime - sunrise) / (sunset - sunrise);
+  const percentage = (currentTime - sunrise) / (sunset - sunrise);
+
+  if (percentage < 0.4) return 1; // Sunrise
+  if (percentage >= 0.4 && percentage <= 0.6) return 2; // Noon
+  if (percentage > 0.6) return 3; // Sunset
+  return 0; // Night
 }
 
-function sunPositionIcon(sunrise, sunset, currentTime) {
-  const percentage = getSunPositionPercentage(sunrise, sunset, currentTime);
+async function getSunPositionIcon(sunrise, sunset, currentTime) {
+  const path = "./img/sun-positions/";
+  const index = getSunPositionIndex(sunrise, sunset, currentTime);
 
-  // ...
+  const currentSunIcon = await import(`${path}sun-${index}.svg`);
+
+  return currentSunIcon.default;
 }
 
-export { sunPositionIcon as sunPositionIndex };
+export default getSunPositionIcon;
