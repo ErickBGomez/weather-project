f307979746c6b0e954c39d811b549f8b81c553c8
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-04-12 22:28:34 -0600

    Create Dialog class

---
+++ src/dialogs.js
@@ -0,0 +1,44 @@
+class Dialog {
+  constructor(id) {
+    this.id = id;
+    this.dialog;
+  }
+
+  setDialogContent(
+    contentCallback,
+    buttons = [{ name: "button", type: "button", label: "Button" }],
+  ) {
+    const dialog = document.createElement("dialog");
+    dialog.id = this.id;
+    const dialogButtons = document.createElement("div");
+    dialogButtons.className = "buttons";
+
+    dialog.appendChild(contentCallback());
+
+    buttons.forEach((button) => {
+      const buttonElement = document.createElement("button");
+      buttonElement.className = button.name;
+      buttonElement.type = button.type;
+      buttonElement.textContent = button.label;
+      dialogButtons.appendChild(buttonElement);
+    });
+
+    dialog.appendChild(dialogButtons);
+
+    this.dialog = dialog;
+    console.log(this.dialog);
+  }
+
+  showDialog() {
+    if (!this.dialog) return;
+    document.body.appendChild(this.dialog);
+    this.dialog.showModal();
+  }
+
+  closeDialog() {
+    this.dialog.close();
+    document.body.removeChild(this.dialog);
+  }
+}
+
+export default Dialog;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./style.css";
 import * as page from "./page";
 import * as forecast from "./forecast";
 import * as settings from "./settings";
+import Dialog from "./dialogs";
 
 const defaultLocation = "San Salvador";
 
@@ -11,3 +12,13 @@ page.renderHeader();
 page.renderMain();
 forecast.setWeather(defaultLocation);
 page.renderFooter();
+
+const newDialog = new Dialog("my-dialog");
+
+newDialog.setDialogContent(() => {
+  const text = document.createElement("p");
+  text.textContent = "Text example";
+  return text;
+}, [{ name: "close", type: "button", label: "Close dialog" }]);
+
+newDialog.showDialog();
