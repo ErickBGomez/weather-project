aec9842b7c900e053f824cf5c2c86eeb95abd507
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-04-14 22:33:21 -0600

    Remove Dialog test from index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import "./style.css";
 import * as page from "./page";
 import * as forecast from "./forecast";
 import * as settings from "./settings";
-import Dialog from "./dialogs";
 
 const defaultLocation = "London";
 
@@ -12,19 +11,3 @@ page.renderHeader();
 page.renderMain();
 forecast.setWeather(defaultLocation);
 page.renderFooter();
-
-const newDialog = new Dialog("my-dialog");
-
-newDialog.setContent(
-  (() => {
-    const text = document.createElement("p");
-    text.textContent = "Text example";
-    return text;
-  })(),
-  [
-    { classes: "ok primary", type: "button", label: "Ok" },
-    { classes: "close secondary", type: "button", label: "Close" },
-  ],
-);
-
-// newDialog.showDialog();
